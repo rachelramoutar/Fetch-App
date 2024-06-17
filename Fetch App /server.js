0882bbdf910658c12697069a56de6090ec976293
Rachel Ramoutar <rachelramoutar1@gmail.com>
2024-06-16 23:35:24 -0400

    setting up INDUCES and .ejs files

--- Fetch App /server.js
+++ Fetch App /server.js
@@ -1,14 +1,19 @@
 const express = require('express')
 const app = express()
+const mongoose = require('mongoose')
 const port = 4000 
 
 const pets= require('./models/fetch.js')
 console.log(pets)
 
+//MIDDLEWARE
+app.use(express.urlencoded({extended: true}))
+
+
 //INDEX
 app.get('/fetch', (req, res)=>{
     res.render('index.ejs', {
-        allPets: pets,
+        pets: allPets
     })
 })
 
@@ -22,18 +27,26 @@ app.get('/fetch/new', (req, res)=>{
 
 
 //UPDATE
-
+app.put('/fetch/:id', (req, res)=>{
+    res.redirect('/fetch')
+})
 
 //CREATE
-
+app.post('/fetch', (req, res)=>{
+    res.redirect('/fetch')
+})
 
 //EDIT
-
+app.get('/fetch/:id/edit', (req, res)=>{
+    res.render('edit.ejs', {
+        fetch: editFetch
+    })
+})
 
 //SHOW
-app.get('/fruits/:index', (req, res)=>{
+app.get('/fruits/:id', (req, res)=>{
     res.render('show.ejs', {
-        pet: pets[req.params.index]
+        pet: pets[req.params.id]
     })
 })
 
