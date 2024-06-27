c7f8266a1cb38c2beb7ccc4f3a9e4800fb0e0e15
Rachel Ramoutar <rachelramoutar1@gmail.com>
2024-06-26 20:14:47 -0400

    added some more CSS

--- Fetch-App/models/fetchPup.js
+++ Fetch-App/models/fetchPup.js
@@ -1,13 +1,14 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-const fetchSchema = new Schema({ 
-    ownerFirstName: {type: String, required: true},
-    ownerLastName: {type: String, required: true},
-    nameOfPet: {type: String, required: true},
-    colorOfPet: {type: String, required: true},
-    sizeOfPet: {type: String, required: true},
-    ageOfPet: {type: String},
+const fetchSchema = new Schema({
+    ownerFirstName: { type: String, required: true },
+    ownerLastName: { type: String, required: true },
+    nameOfPet: { type: String, required: true },
+    colorOfPet: { type: String, required: true },
+    sizeOfPet: { type: String, required: true },
+    ageOfPet: { type: String },
+    dateOfBirth: { type: Date },
 })
 
 const FetchPup = mongoose.model('FetchPup', fetchSchema)
--- Fetch-App/server.js
+++ Fetch-App/server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const multer = require('multer');
-const upload = multer({dest: 'uploads/'});
+const upload = multer({ dest: 'uploads/' });
 const app = express();
 const mongoose = require('mongoose');
 const methodOverride = require('method-override');
@@ -12,15 +12,15 @@ const port = process.env.PORT || 3500;
 
 const mongoURI = process.env.MONGOURI;
 
-app.get('/', (req, res)=>{
+app.get('/', (req, res) => {
     res.redirect('/fetch')
 });
 
-async function connectToMongo(){
-    try{
+async function connectToMongo() {
+    try {
         await mongoose.connect(mongoURI)
         console.log(`I'm connected to mongoDB`)
-    } catch (err){
+    } catch (err) {
         console.error(err)
     }
 }
@@ -30,13 +30,13 @@ connectToMongo()
 //MIDDLEWARE
 app.use(express.static('public'))
 app.use(express.json())
-app.use(express.urlencoded({extended: true}))
+app.use(express.urlencoded({ extended: true }))
 app.use(methodOverride("_method"))
 app.use('/fetch', fetchController)
 // app.use('/fetchUser', userController)
 
 
-app.listen(port, ()=>{
+app.listen(port, () => {
     console.log(`listening on ${port}`)
 })
 
